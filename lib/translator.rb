78ccfeb3fe46ad9a8231ab8cfc24450414afcbb2
Arina Victor <arinavictor@yahoo.com>
2019-08-22 21:20:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'yaml'
 require 'pry'
 
 def load_library(emoticon_file)
-  # code goes here
+  
  emoticons = YAML.load_file("./lib/emoticons.yml")
   emojis = {'get_meaning' => {}, 
             'get_emoticon' => {} }
@@ -19,7 +19,8 @@ emojis
 end
 
 def get_japanese_emoticon(emoticon_file, emoticon)
-  # code goes here
+  
+ 
 end
 
 def get_english_meaning
